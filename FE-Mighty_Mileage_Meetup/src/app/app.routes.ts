29b0ca47a31ecd85e56bf749cecddfd2238fdd82
sjtroxel <sjtroxel@protonmail.com>
2025-08-29 15:07:15 -0500

    blank dashboard and signup components to uncomment routes

--- FE-Mighty_Mileage_Meetup/src/app/app.routes.ts
+++ FE-Mighty_Mileage_Meetup/src/app/app.routes.ts
@@ -3,24 +3,24 @@ import { authGuard } from './auth-guard';
 import { noAuthGuard } from './no-auth-guard';
 
 export const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadComponent: () =>
-  //     import('./pages/dashboard/dashboard').then((c) => c.DashboardComponent),
-  //   canActivate: [authGuard],
-  // },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard').then((c) => c.DashboardComponent),
+    canActivate: [authGuard],
+  },
   {
     path: 'login',
     loadComponent: () =>
       import('./pages/login/login').then((c) => c.LoginComponent),
     canActivate: [noAuthGuard],
   },
-  // {
-  //   path: 'signup',
-  //   loadComponent: () =>
-  //     import('./pages/signup/signup').then((c) => c.SignupComponent),
-  //   canActivate: [noAuthGuard],
-  // },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./pages/signup/signup').then((c) => c.SignupComponent),
+    canActivate: [noAuthGuard],
+  },
   {
     path: '**',
     redirectTo: '',
---
+++ FE-Mighty_Mileage_Meetup/src/app/pages/signup/signup.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  imports: [],
+  standalone: true,
+  templateUrl: './signup.html',
+  styleUrl: './signup.scss'
+})
+export class SignupComponent {
+
+}
