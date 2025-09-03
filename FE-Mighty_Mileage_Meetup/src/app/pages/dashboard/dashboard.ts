d6f6d088f3ce0d568ae65bf25bd4ea9c0d72c377
sjtroxel <sjtroxel@protonmail.com>
2025-09-03 12:30:53 -0500

    working on dashboard template where existing meetups are to be displayed

--- FE-Mighty_Mileage_Meetup/src/app/pages/dashboard/dashboard.ts
+++ FE-Mighty_Mileage_Meetup/src/app/pages/dashboard/dashboard.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MeetupService } from '../../services/meetup';
+import { Meetup } from '../../shared/models/meetup';
+import { Location } from '../../shared/models/location';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
-export class DashboardComponent {
 
+export class DashboardComponent implements OnInit {
+  meetupService = inject(MeetupService);
+
+  meetups = signal<Meetup[]>([]);
+  loading = signal(true);
+
+  showModal = signal(false);
+  meetupToEdit = signal<Meetup | null>(null);
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.loadMeetups();
+  }
+
+  loadMeetups(): void {
+    this.loading.set(true);
+    this.meetupService.getAllMeetups().subscribe({
+      next: (data) => {
+        this.meetups.set(data);
+        this.loading.set(false);
+      },
+      error: (err) => {
+        console.error('Error fetching meetups', err);
+        this.loading.set(false);
+      }
+    });
+  }
+
+  openModal(meetup?: Meetup): void {
+    if (meetup) {
+      this.meetupToEdit.set(meetup);
+    } else {
+      this.meetupToEdit.set(null);
+    }
+    this.showModal.set(true);
+  }
+
+  closeModal(): void {
+    this.showModal.set(false);
+    this.meetupToEdit.set(null);
+  }
+
+  deleteMeetup(meetupId: number): void {
+    this.meetupService.deleteMeetup(meetupId).subscribe(() => {
+      this.meetups.set(this.meetups().filter(m => m.id !== meetupId));
+    });
+  }
 }
--- FE-Mighty_Mileage_Meetup/src/app/shared/models/meetup.ts
+++ FE-Mighty_Mileage_Meetup/src/app/shared/models/meetup.ts
@@ -1,9 +1,12 @@
+import { Location as AppLocation } from './location';
+
 export interface Meetup {
   id: number;
+  title?: string;
   activity: 'run' | 'bicycle';
   start_date_time: string;
   end_date_time: string;
   guests: number;
   user_id: number;
-  location?: Location;
+  location?: AppLocation;
 }
